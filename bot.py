b38c7643786db2c93c1d1b6c510036c06c17648c
muqaddasmuzaffarova37-tech <muqaddasmuzaffarova37@gmail.com>
2025-12-14 16:51:46 +0500

    Add bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,31 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, ChannelPostHandler
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHANNEL_ID = os.environ.get("CHANNEL_ID")
+
+if not BOT_TOKEN or not CHANNEL_ID:
+    raise RuntimeError("BOT_TOKEN yoki CHANNEL_ID topilmadi")
+
+CHANNEL_ID = int(CHANNEL_ID)
 
 async def react_to_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.channel_post and update.channel_post.chat.id == CHANNEL_ID:
+    post = update.channel_post
+    if post and post.chat.id == CHANNEL_ID:
         try:
             await context.bot.set_message_reaction(
                 chat_id=CHANNEL_ID,
-                message_id=update.channel_post.message_id,
+                message_id=post.message_id,
                 reaction="👍"
             )
         except Exception as e:
             print("Reaction error:", e)
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(ChannelPostHandler(react_to_post))
-app.run_polling()
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(ChannelPostHandler(react_to_post))
+    print("Bot started")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
